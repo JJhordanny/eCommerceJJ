0a6c82a95925911b0d03a9d802bb0b07e103ccfd
Jhordanny Hernández <j.jhordanny18@hotmail.com>
2016-07-17 11:42:51 -0400

    Agregando primer EJS para la creación de usuarios

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var ejs-mate = require('ejs-mate');
+
+var User = require('./models/user')
 
 var app = express();
 
@@ -8,23 +13,28 @@ mongoose.connect('mongodb://root:root@ds040089.mlab.com:40089/ecommerce', functi
   if (err) {
     console.log(err);
   } else {
-    console.log("Connected to the database");
+    console.log("Conectado a la Base de Datos");
   }
 });
 
 //Adding middleware
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-  var name = "Batman";
-  res.json("My name is " + name);
+app.post('/create-user', function(req, res, next){
+  var user = new User();
+  user.profile.name = req.body.name;
+  user.password = req.body.password;
+  user.email = req.body.email;
+
+  user.save(function(err){
+    if (err) return next(err);
+    res.json('Nuevo usuario creado exitosamente!');
+  });
 });
-
-app.get('/jj', function (req, res){
-  res.json("My name is JJ!")
-})
 
 app.listen(3000, function (err) {
   if (err) throw err;
-  console.log("Server is running on port 3000");
+  console.log("Servidor esta corriendo en el puerto 3000");
 });
