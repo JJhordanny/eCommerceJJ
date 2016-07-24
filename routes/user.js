ecfb38baa750f5f751ca3ccd923489922771ab3e
Jhordanny Hernández <j.jhordanny18@hotmail.com>
2016-07-24 13:31:46 -0400

    Adding code of the signup session

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 var router = require('express').Router();
 var User = require('../models/user');
 
-router.get('/singup', function(req, res){
-  res.render('/singup');
+router.get('/signup', function(req, res){
+  res.render('accounts/signup');
 });
 
-router.post('/singup', function(req, res, next){
+router.post('/signup', function(req, res, next){
   var user = new User();
 
   user.profile.name = req.body.name;
@@ -15,7 +15,7 @@ router.post('/singup', function(req, res, next){
   User.findOne({ email: req.body.email}, function(err, existUser){
     if (existUser) {
       console.log(req.body.email + " existe!");
-      return res.redirect('/singup')
+      return res.redirect('/signup')
     }
     else {
       user.save(function(err, user){
