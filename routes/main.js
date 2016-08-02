136d486b64584d397197cd032b77fd1e6f02c330
Jhordanny Hernández <j.jhordanny18@hotmail.com>
2016-08-02 18:08:04 -0400

    Adding Search feature

--- routes/main.js
+++ routes/main.js
@@ -27,6 +27,28 @@ stream.on('error', function(err) {
   console.log(err);
 });
 
+router.post('/search', function(req, res, next) {
+  res.redirect('/search?q=' + req.body.q);
+});
+
+router.get('/search', function(req, res, next){
+  if (req.query.q) {
+    Product.search({
+      query_string: { query: req.query.q}
+    }, function(err, results) {
+      results:
+      if (err) return next(err);
+      var data = results.hits.hits.map(function(hit) {
+        return hit;
+      });
+      res.render('main/search-result', {
+        query: req.query.q,
+        data: data
+      });
+    });
+  }
+});
+
 router.get('/', function(req, res){
   res.render('main/home');
 });
