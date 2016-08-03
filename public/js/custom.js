34831fa9c8fa4a9984243c2f0c7b5af504daa48a
Jhordanny Hernández <j.jhordanny18@hotmail.com>
2016-08-02 20:22:33 -0400

    Instant search complete

--- public/js/custom.js
+++ public/js/custom.js
@@ -15,11 +15,25 @@ $(function() {
           return hit;
         });
 
-        console.log(data);
+        $('#searchResults').empty();
+        for (var i = 0; i < data.length; i++) {
+          var html = "";
+          html += '<div class="col-md-4">';
+          html += '<a href="/product/' + data[i]._source._id + '">';
+          html += '<div class="thumbnail">';
+          html += '<img src="' + data[i]._source.image + '"/>';
+          html += '<div class="caption">';
+          html += '<h3><span style="font-weight: bold">' + data[i]._source.name + '</span></h3>';
+          html += '<h3>' + data[i]._source.category.name + '</h3>';
+          html += '<h3>$' + data[i]._source.price + '</h3>';
+          html += '</div></div></a></div>';
+
+          $('#searchResults').append(html);
+        }
       },
 
       error: function(error) {
-        console.log(error);
+        console.log(err);
       }
     });
   });
