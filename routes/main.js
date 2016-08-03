00b47a32e0a157bb999b4767ffcabbbc53b06d68
Jhordanny Hernández <j.jhordanny18@hotmail.com>
2016-08-03 17:00:26 -0400

    Adding Remove feature to remove from cart list

--- routes/main.js
+++ routes/main.js
@@ -57,7 +57,8 @@ router.get('/cart', function(req, res, next) {
     .exec(function(err, foundCart) {
       if (err) return next(err);
       res.render('main/cart', {
-        foundCart: foundCart
+        foundCart: foundCart,
+        message: req.flash('remove')
       });
     });
 });
@@ -74,6 +75,19 @@ router.post('/product/:product_id', function(req, res, next) {
 
     cart.save(function(err) {
       if (err) return next(err);
+      return res.redirect('/cart');
+    });
+  });
+});
+
+router.post('/remove', function(req, res, next) {
+  Cart.findOne({ owner: req.user._id }, function(err, foundCart) {
+    foundCart.items.pull(String(req.body.item));
+
+    foundCart.total = (foundCart.total - parseFloat(req.body.price)).toFixed(2);
+    foundCart.save(function(err, found) {
+      if (err) return next(err);
+      req.flash('remove', 'Eliminado del Carrito');
       res.redirect('/cart');
     });
   });
